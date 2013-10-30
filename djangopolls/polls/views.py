a478e350fd7e84e9db3adc528ad17f5a61c5eef0
Lowell Bander <lowellbander@gmail.com>
2013-10-30 16:01:30 -0700

    finished part 3

--- djangopolls/polls/views.py
+++ djangopolls/polls/views.py
@@ -1,21 +1,15 @@
-# Create your views here.
-from django.http import HttpResponse
-from django.template import RequestContext, loader
+from django.http import HttpResponse, Http404
+from django.shortcuts import render, get_object_or_404
 from polls.models import Poll
 
-def index(request): # takes an HTTP Request (verb(?)) as an argument
-	latest_poll_list = Poll.objects.order_by('-pub_date')[:5] # first five
-	# latest_polls_list = Poll.objects.all()
-	# output = ', '.join([p.question for p in latest_polls_list])
-	template = loader.get_template('polls/index.html')
-	context = RequestContext(request, {
-		'latest_poll_list' : latest_poll_list,
-	})
-	# return HttpResponse("asdasd")
-	return HttpResponse(template.render(context))
+def index(request):
+	latest_poll_list = Poll.objects.order_by('-pub_date')[:5]
+	context = {'latest_poll_list' : latest_poll_list}
+	return render(request, 'polls/index.html', context)
 
 def detail(request, poll_id):
-	return HttpResponse('You\'re looking at poll %s.' % poll_id)
+	poll = get_object_or_404(Poll, pk=1)
+	return render(request, 'polls/detail.html', {'poll' : poll})
 
 def results(request, poll_id):
 	return HttpResponse('You\'re looking at the results of poll %s.' % poll_id)
